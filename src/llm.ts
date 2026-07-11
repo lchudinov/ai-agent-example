0a5d7d705d5652fa8663645da7f1d504d2889843
Leonty Chudinov <leonty.chudinov@gmail.com>
2026-07-11 10:45:30 +0500

    Call the date tool

--- src/llm.ts
+++ src/llm.ts
@@ -1,6 +1,5 @@
-import OpenAI from "openai";
 import { Tool } from "./tool.js";
-import { OpenRouter,  } from '@openrouter/sdk';
+import { OpenRouter, } from '@openrouter/sdk';
 import { ChatMessages } from "@openrouter/sdk/models";
 
 const openRouter = new OpenRouter({
@@ -12,24 +11,63 @@ export async function runLLM(
   tools: Tool[]
 ) {
 
-  const completion = await openRouter.chat.send({chatRequest: {
-    model: 'openrouter/free',
-    messages,
-    tools: tools.map(tool => ({
-      type: "function",
-      function: {
-        name: tool.name,
-        description: tool.description,
-        parameters: {
-          type: "object",
-          properties: {},
-          additionalProperties: true
+  const completion = await openRouter.chat.send({
+    chatRequest: {
+      model: 'openrouter/free',
+      messages,
+      tools: tools.map(tool => ({
+        type: "function",
+        function: {
+          name: tool.name,
+          description: tool.description,
+          parameters: {
+            type: "object",
+            properties: {},
+            additionalProperties: true
+          }
         }
+      })),
+      stream: false,
+    }
+  });
+  const response_1 = completion.choices[0].message;
+  messages.push(response_1);
+  const { toolCalls } = response_1;
+  if (toolCalls) {
+    for (const toolCall of toolCalls) {
+      const toolName = toolCall.function.name;
+      const tool = tools.find(t => t.name === toolName);
+      if (!tool) {
+        console.log(`tool ${toolName} not found`);
+        return response_1;
       }
-    })),
-    stream: false,
+      //const { search_params } = JSON.parse(toolCall.function.arguments);
+      const toolResponse = await tool.execute({});
+      messages.push({
+        role: 'tool',
+        toolCallId: toolCall.id,
+        content: JSON.stringify(toolResponse),
+      });
+    }
+    const response_2 = await openRouter.chat.send({
+      chatRequest: {
+        model: 'openrouter/free',
+        messages,
+        tools: tools.map(tool => ({
+          type: "function",
+          function: {
+            name: tool.name,
+            description: tool.description,
+            parameters: {
+              type: "object",
+              properties: {},
+              additionalProperties: true
+            }
+          }
+        })),
+        stream: false,
+      }
+    });
+    return response_2.choices[0].message;
   }
-});
-  return completion.choices[0].message;
-
 }
